749863d787ace0c0b67b3c3f9762521fef455a1f
Rafael Veiga Ramos <117952569+FireWars@users.noreply.github.com>
2023-05-10 19:34:27 -0300

    Add files via upload

--- js/crud.js
+++ js/crud.js
@@ -1,16 +1,16 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
-let tarefas = []
+let questoes = []
 
 window.addEventListener("load", () => {
-    tarefas = JSON.parse( localStorage.getItem("tarefas") ) || []
+    questoes = JSON.parse(localStorage.getItem("questoes")) || []
     atualizar()
 })
 
-function atualizar(){
-    document.querySelector("#tarefas").innerHTML = ""
-    tarefas.forEach(tarefa => 
-        document.querySelector("#tarefas").innerHTML += criarCard(tarefa))
+function atualizar() {
+    document.querySelector("#questoes").innerHTML = ""
+    questoes.forEach(questao =>
+        document.querySelector("#questoes").innerHTML += criarCard(questao))
 }
 
 function cadastrar() {
@@ -19,28 +19,28 @@ function cadastrar() {
     const categoria = document.querySelector("#categoria").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-    const tarefa = { //JSON Java Script Object Notation
+    const questao = { //JSON Java Script Object Notation
         titulo,
         pontos,
         categoria
     }
 
-    if (!isValid(tarefa.titulo, document.querySelector("#titulo"))) return
-    if (!isValid(tarefa.pontos, document.querySelector("#pontos"))) return
+    if (!isValid(questao.titulo, document.querySelector("#titulo"))) return
+    if (!isValid(questao.pontos, document.querySelector("#pontos"))) return
 
-    tarefas.push(tarefa)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    questoes.push(questao)
+    localStorage.setItem("questoes", JSON.stringify(questoes))
 
     atualizar()
     modal.hide()
 }
 
-function isValid(valor, campo){
-    if(valor.length == 0){
+function isValid(valor, campo) {
+    if (valor.length == 0) {
         campo.classList.add("is-invalid")
         campo.classList.remove("is-valid")
         return false
-    }else{
+    } else {
         campo.classList.add("is-valid")
         campo.classList.remove("is-invalid")
         return true
@@ -48,47 +48,32 @@ function isValid(valor, campo){
 
 }
 
-function apagar(botao){
+function apagar(botao) {
     botao.parentNode.parentNode.parentNode.remove()
 }
 
-function criarCard(tarefa) {
+function criarCard(questao) {
     const card = `
-    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="col-lg-3 col-md-6 col-sm-12">
     <div class="card">
         <div class="card-header">
-            Pergunta 1
+            ${questao.titulo}
         </div>
         <div class="card-body">
-            <p class="card-text">Qual é o nome do primeiro álbum solo da Beyoncé?</p>
-            <form>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="quiz1" id="quiz1Option1" value="option1">
-                    <label class="form-check-label" for="quiz1Option1">
-                        Dangerously in Love
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="quiz1" id="quiz1Option2" value="option2">
-                    <label class="form-check-label" for="quiz1Option2">
-                        Renaissance
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="quiz1" id="quiz1Option2" value="option2">
-                    <label class="form-check-label" for="quiz1Option2">
-                        B'Day
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="quiz1" id="quiz1Option2" value="option2">
-                    <label class="form-check-label" for="quiz1Option2">
-                        Lemonade
-                    </label>
-                </div>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text">${questao.categoria}</p>
+            <span class="badge text-bg-warning">${questao.pontos}pt</span>
         </div>
-    </div>
-</div>
-    ` 
+        <div class="card-footer">
+            <a href="#" class="btn btn-success" title="marcar como concluída">
+                <i class="bi bi-check2"></i>
+            </a>
+            <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar questao">
+                <i class="bi bi-trash3"></i>
+            </a>
+        </div> <!-- card footer -->
+    </div> <!-- card -->
+</div> <!-- col -->
+    `
     return card
 }
